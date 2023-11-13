38ec4b608fa4416fb6fa6a40266bf96495b619c3
Manoj-kumar-ravirajan <manoj.ravirajan@rapido.bike>
2023-11-13 23:09:25 +0530

    commit all local files

---
+++ customer-retention/jira/CUR-2553/LTR x Recency Performance Tracking | City-wise |TAXI.sql
@@ -0,0 +1,412 @@
+WITH base_month_customer AS 
+(
+SELECT 
+    DATE_FORMAT(DATE_TRUNC('month', date(run_date)), '%Y-%m-%d') AS month,
+    taxi_lifetime_last_ride_city city,
+    customer_id,
+    --MAX(taxi_lifetime_last_ride_date) 
+    taxi_lifetime_last_ride_date,
+    --MAX(taxi_lifetime_rides) 
+    taxi_lifetime_rides
+
+FROM datasets.iallocator_customer_segments
+WHERE 
+    run_date BETWEEN DATE_FORMAT(DATE_TRUNC('month', {{start_date}}), '%Y-%m-%d')
+    AND DATE_FORMAT(DATE_TRUNC('month', {{end_date}}) , '%Y-%m-%d') 
+    AND run_date = DATE_FORMAT(DATE_TRUNC('month', date(run_date)), '%Y-%m-%d')
+    AND (taxi_lifetime_rides > 0 or taxi_lifetime_rides is not null or taxi_lifetime_last_ride_date is not null)
+    AND [[ {{city}} --]] taxi_lifetime_last_ride_city = 'Delhi'
+--GROUP BY 1,2
+),
+
+base_month_segment AS
+(
+SELECT 
+    DATE_FORMAT(DATE_TRUNC('month', DATE(month)) , '%Y-%m-%d') AS month,
+    city,
+    customer_id,
+    CASE
+    WHEN taxi_lifetime_rides IS NULL THEN 'LTR=0'
+    WHEN taxi_lifetime_rides BETWEEN 1 AND 4 THEN '1-4'
+    WHEN (taxi_lifetime_rides BETWEEN 5 AND 20) AND (taxi_lifetime_last_ride_date >= (DATE_TRUNC('month', DATE(month)) - INTERVAL '30' DAY)) THEN '5-20'
+    WHEN (taxi_lifetime_rides BETWEEN 21 AND 50) AND (taxi_lifetime_last_ride_date >= (DATE_TRUNC('month', DATE(month)) - INTERVAL '30' DAY)) THEN '21-50'
+    WHEN (taxi_lifetime_rides > 50) AND (taxi_lifetime_last_ride_date >= (DATE_TRUNC('month', DATE(month)) - INTERVAL '30' DAY)) THEN '50+'
+    WHEN taxi_lifetime_rides >= 5 THEN '>=5'
+    ELSE 'LTR=0'
+    END ltr_taxi,
+    CASE 
+    WHEN taxi_lifetime_last_ride_date IS NULL THEN 'LTR=0'
+    WHEN taxi_lifetime_last_ride_date >= (DATE_TRUNC('month', DATE(month)) - INTERVAL '30' DAY) THEN '1. LAST_30_DAYS'
+    WHEN taxi_lifetime_last_ride_date BETWEEN (DATE_TRUNC('month', DATE(month)) - INTERVAL '90' DAY) AND (DATE_TRUNC('month', DATE(month)) - INTERVAL '31' DAY) THEN '2. 31_90_DAYS'
+    WHEN taxi_lifetime_last_ride_date BETWEEN (DATE_TRUNC('month', DATE(month)) - INTERVAL '180' DAY) AND (DATE_TRUNC('month', DATE(month)) - INTERVAL '91' DAY) THEN '3. 91_180_DAYS'
+    WHEN taxi_lifetime_last_ride_date BETWEEN (DATE_TRUNC('month', DATE(month)) - INTERVAL '365' DAY) AND (DATE_TRUNC('month', DATE(month)) - INTERVAL '181' DAY) THEN '4. 181_365_DAYS'
+    WHEN taxi_lifetime_last_ride_date < (DATE_TRUNC('month', DATE(month)) - INTERVAL '365' DAY) THEN '5. 365_ABOVE'
+    ELSE 'LTR=0'
+    END recency_taxi
+FROM base_month_customer
+),
+
+base_month_total AS
+(
+SELECT 
+    DATE_FORMAT(DATE_TRUNC('month', DATE(month)) , '%Y-%m-%d') AS month,
+    city,
+    COUNT(DISTINCT customer_id) existing_custr_total 
+FROM base_month_customer
+GROUP BY 1,2
+),
+
+customer_rf_daily_kpi AS
+(
+SELECT 
+    day,
+    city,
+    customerid,
+    rr_sessions_unique_daily,
+    net_rides_daily,
+    ao_sessions_unique_daily,
+    fe_sessions_unique_daily,
+    discount_daily,
+    subtotal_daily
+        
+FROM datasets.customer_rf_daily_kpi
+WHERE 
+    day BETWEEN DATE_FORMAT(DATE_TRUNC('month', {{start_date}}), '%Y-%m-%d')
+    AND DATE_FORMAT(DATE_TRUNC('month', {{end_date}} + INTERVAL '28' DAY) - INTERVAL '1' DAY, '%Y-%m-%d')
+    AND service_name IN ('Link', 'Auto')
+    AND customerid IS NOT NULL
+    AND city IN (SELECT DISTINCT city FROM base_month_customer )
+),
+
+rr_net AS
+(
+SELECT 
+    DATE_FORMAT(DATE_TRUNC('month', DATE(day)), '%Y-%m-%d') AS month,
+    city,
+    customerid as customer_id,
+    SUM(rr_sessions_unique_daily) rr,
+    SUM(net_rides_daily) net_rides,
+    SUM(discount_daily) burn,
+    SUM(subtotal_daily) subtotal
+FROM customer_rf_daily_kpi
+WHERE 
+    day BETWEEN DATE_FORMAT(DATE_TRUNC('month', {{start_date}}), '%Y-%m-%d') 
+    AND DATE_FORMAT(DATE_TRUNC('month', {{end_date}} + INTERVAL '28' DAY) - INTERVAL '1' DAY, '%Y-%m-%d')
+GROUP BY 1,2,3
+),
+
+max_daily_ao AS
+(
+SELECT 
+        day,
+        city,
+        customer_id,
+        SUM(ao) AS ao,
+        SUM(fe) AS fe
+FROM
+    (
+    SELECT 
+        customerid as customer_id,
+        city,
+        day,
+        max(ao_sessions_unique_daily) AS ao,
+        max(fe_sessions_unique_daily) AS fe
+    FROM datasets.customer_rf_daily_kpi
+    WHERE 
+        day BETWEEN DATE_FORMAT(DATE_TRUNC('month', {{start_date}}), '%Y-%m-%d') 
+        AND DATE_FORMAT(DATE_TRUNC('month', {{end_date}} + INTERVAL '28' DAY) - INTERVAL '1' DAY, '%Y-%m-%d')
+        AND customerid IS NOT NULL
+        AND city IN (SELECT DISTINCT city FROM base_month_customer )
+    GROUP BY 1,2,3
+    )
+GROUP BY 1,2,3
+),
+
+monthly_ao AS
+(
+SELECT  
+    DATE_FORMAT(DATE_TRUNC('month', DATE(day)), '%Y-%m-%d') AS month,
+    city,
+    customer_id,
+    SUM(ao) AS ao,
+    SUM(fe) AS fe
+FROM max_daily_ao
+GROUP BY 1,2,3
+),
+
+orders as
+(
+select *,
+    DATE_FORMAT(DATE_TRUNC('month', DATE(date_parse(yyyymmdd,'%Y%m%d'))), '%Y-%m-%d') Start,
+    service_obj_city_display_name city2
+from orders.order_logs_snapshot
+where service_obj_service_name in ('Auto','Link')
+    and service_obj_city_display_name IN (SELECT DISTINCT city FROM base_month_customer )
+    and order_id is not null
+    and service_obj_city_display_name is not null
+    and yyyymmdd between DATE_FORMAT(DATE_TRUNC('month', {{start_date}})- interval '14' day,'%Y%m%d') 
+    and DATE_FORMAT(DATE_TRUNC('month', {{end_date}} + INTERVAL '28' DAY) - INTERVAL '1' DAY, '%Y%m%d')
+),
+
+order_coin as
+(
+select
+    DATE_FORMAT(DATE_TRUNC('month', DATE(date_parse(yyyymmdd,'%Y%m%d'))), '%Y-%m-%d') StartDate,
+    owner_id customerid,
+    entity_id,
+    cast(json_extract(coin_wallet_changes, '$[0].offerType') as varchar) offerType,
+    coalesce(round(sum(case when transaction_type = 'credit' then cast(amount as double) end)),0) as coin_credited,
+    coalesce(round(sum(case when transaction_type = 'debit' and subtype != 'coinExpired' then cast(amount as double) end)),0) as coin_utilized,
+    coalesce(round(sum(case when transaction_type = 'debit' and subtype = 'coinExpired' then cast(amount as double) end)),0) as coin_expired
+from payments.transactions_snapshot
+where yyyymmdd between DATE_FORMAT(DATE_TRUNC('month', {{start_date}}),'%Y%m%d') 
+    and DATE_FORMAT(DATE_TRUNC('month', {{end_date}} + INTERVAL '28' DAY) - INTERVAL '1' DAY, '%Y%m%d')
+    and owner_type ='customer'
+    and transaction_status = 'done'
+    and json_extract(coin_wallet_changes, '$[0].id') is not null
+group by 1,2,3,4
+),
+
+service_total_coins as
+(
+select
+    StartDate,
+    customerid,
+    city2,
+    entity_id,
+    sum(case when offerType in ('locationOffer','rideOffer','scratchCardOffer') and service_obj_service_name = 'Auto' then coin_credited end) total_coin_credited,
+    sum(case when offerType in ('locationOffer','rideOffer','scratchCardOffer') and service_obj_service_name = 'Auto' then coin_expired end) total_coin_expired,
+    sum(case when offerType = 'giftOffer' and service_obj_service_name = 'Auto' then coin_utilized end) giftOffer_utilized,
+    sum(case when offerType = 'walletRechargeOffer' and service_obj_service_name = 'Auto' then coin_utilized end) walletOffer_utilized,
+    sum(case when offerType = 'dashboardCoinsCredit' and service_obj_service_name = 'Auto' then coin_utilized end) dashboardCoinsCredit_utilized
+from
+    (
+    select a.*, b.*
+    from order_coin a join orders b on a.customerid = b.customer_id and a.entity_id = b.order_id and a.StartDate = b.Start
+    )
+where StartDate is not null
+group by 1,2,3,4
+),
+
+coin_credit as
+(
+select
+    Start,
+    city,
+    customerid,
+    sum(total_coin_credited) total_coin_credited,
+    sum(walletOffer_utilized) walletOffer_utilized,
+    sum(giftOffer_utilized) giftOffer_utilized,
+    sum(dashboardCoinsCredit_utilized) dashboardCoinsCredit_utilized
+from
+    (
+    select
+        (case when a.city is null then b.city2 else a.city end) as city,
+        (case when a.customer_id is null then b.customerid else a.customer_id end) as customerid,
+        (case when a.month is null then b.StartDate else a.month end) as Start,
+        b.walletOffer_utilized,
+        b.giftOffer_utilized,
+        b.dashboardCoinsCredit_utilized,
+        b.total_coin_credited
+    from rr_net a 
+    right join service_total_coins b on a.month = b.StartDate and a.customer_id = b.customerid and a.city = b.city2
+    )
+where Start is not null
+group by 1,2,3
+),
+
+coin_expired_customer_level as
+(
+select
+    StartDate as Start2,
+    city2,
+    customerid,
+    sum(coin_expired) coin_expired
+from order_coin a 
+join orders b on a.entity_id = b.order_id 
+where coin_expired > 0
+group by 1,2,3
+),
+
+agg_coin as
+(
+select
+    a.*,
+    b.coin_expired,
+    (coalesce(a.total_coin_credited,0) + coalesce(a.walletOffer_utilized,0) + coalesce(giftOffer_utilized,0) + coalesce(a.dashboardCoinsCredit_utilized,0) - coalesce(b.coin_expired,0)) total_coin_burn
+from coin_credit a 
+join coin_expired_customer_level b on a.Start = b.Start2 and a.city = b.city2 and a.customerid = b.customerid
+),
+
+base_current AS (
+SELECT 
+        COALESCE(b.city,c.city,r.city) city,
+        COALESCE(b.month,c.month,r.month) month,
+        -- b.month,
+        COALESCE(b.customer_id,c.customer_id,r.customer_id) customers,
+        b.customer_id bcustomer_id,
+        COALESCE(b.ltr_taxi,'NEW') ltr_taxi,
+        COALESCE(b.recency_taxi, 'NEW') recency_taxi,
+        c.customer_id acustomer_id,
+        r.customer_id rcustomer_id,
+        CASE 
+        WHEN c.ao = 0 AND r.net_rides > 0 THEN r.net_rides
+        WHEN c.ao = 0 AND c.fe > 0 THEN c.fe
+        WHEN c.ao = 0 AND r.rr > 0 THEN r.rr
+        WHEN c.ao IS NULL THEN 0
+        ELSE c.ao END ao,
+        
+        CASE
+        WHEN c.fe = 0 AND r.net_rides > 0 THEN r.net_rides 
+        WHEN c.fe = 0 AND r.rr > 0 THEN r.rr
+        WHEN c.fe IS NULL THEN 0
+        ELSE c.fe END fe,
+        
+        CASE
+        WHEN r.rr = 0 AND r.net_rides > 0 THEN r.net_rides
+        WHEN r.rr IS NULL THEN 0
+        ELSE r.rr END rr,
+        
+        COALESCE(r.net_rides, 0) net_rides,
+        COALESCE(r.burn, 0) burn,
+        COALESCE(ac.total_coin_burn, 0) total_coin_burn,
+        COALESCE(r.subtotal, 0) subtotal
+
+FROM base_month_segment AS b
+FULL JOIN monthly_ao AS c ON b.customer_id = c.customer_id AND b.month = c.month AND b.city = c.city
+FULL JOIN rr_net AS r ON b.customer_id = r.customer_id AND c.month = r.month AND b.city = r.city
+LEFT JOIN agg_coin ac ON r.customer_id = ac.customerid AND r.month = ac.Start AND r.city = ac.city
+
+),
+
+detailed_view AS 
+(
+SELECT 
+        c.month,
+        c.city,
+        ltr_taxi,
+        case 
+        when ltr_taxi = '1-4' then 1
+        when ltr_taxi = '5-20' then 2
+        when ltr_taxi = '21-50' then 3
+        when ltr_taxi = '50+' then 4
+        when ltr_taxi = '>=5' then 5
+        when ltr_taxi = 'LTR=0' then 6
+        when ltr_taxi = 'NEW' then 7
+        end row_order,
+        recency_taxi,
+        COUNT(DISTINCT bcustomer_id) existing_custr_count,
+        COUNT(DISTINCT CASE WHEN ao > 0 THEN acustomer_id END) ao_users,
+        COUNT(DISTINCT CASE WHEN fe > 0 THEN acustomer_id END) fe_users,
+        COUNT(DISTINCT CASE WHEN rr > 0 THEN rcustomer_id END) rr_users,
+        COUNT(DISTINCT CASE WHEN net_rides > 0 THEN rcustomer_id END) net_users,
+        SUM(net_rides) net_rides,
+        SUM(burn)+SUM(total_coin_burn) discount_with_coin,
+        SUM(subtotal) subtotal
+        
+FROM base_current c
+GROUP BY 1,2,3,4,5
+),
+
+summary_view AS (
+SELECT 
+        c.month,
+        c.city,
+        COUNT(DISTINCT CASE WHEN net_rides > 0 THEN c.customers END) net_users,
+        SUM(net_rides) net_rides,
+        SUM(burn)+SUM(total_coin_burn) discount_with_coin,
+        SUM(subtotal) subtotal
+FROM base_current c
+GROUP BY 1,2
+),
+
+a as 
+(
+SELECT  
+        a.month,
+        a.city,
+        a.row_order,
+        a.ltr_taxi,
+        a.recency_taxi,
+        a.existing_custr_count,
+        COALESCE(try(a.existing_custr_count*100.00/b.existing_custr_total), 0) existing_custr_distr,
+        a.ao_users,
+        COALESCE(try(a.ao_users*100.00/a.existing_custr_count), 0) ao_conversion, 
+        a.fe_users,
+        COALESCE(try(a.fe_users*100.00/a.existing_custr_count), 0) fe_conversion, 
+        a.rr_users,
+        COALESCE(try(a.rr_users*100.00/a.existing_custr_count), 0) rr_conversion,
+        COALESCE(try(a.rr_users*100.00/a.fe_users), 0) fe_rr,
+        a.net_users,
+        COALESCE(try(a.net_users*100.00/a.existing_custr_count), 0) net_conversion,
+        COALESCE(try(a.net_users*100.00/a.rr_users), 0) rr_net,
+        COALESCE(try(a.net_users*100.00/c.net_users), 0) net_users_distr,
+        a.net_rides,
+        COALESCE(try(a.net_rides*100.00/c.net_rides), 0) net_rides_distr,
+        COALESCE(try(a.net_rides*1.00/a.net_users), 0) rpc,
+        a.discount_with_coin,
+        a.subtotal,
+        COALESCE(try(a.discount_with_coin*100.0/a.subtotal), 0) discount_perc,
+        COALESCE(try(a.discount_with_coin*1.0/a.net_rides), 0) dpr
+        
+FROM detailed_view a
+LEFT JOIN base_month_total b ON a.month = b.month AND a.city = b.city
+LEFT JOIN summary_view c ON a.month = c.month AND a.city = c.city
+ORDER BY 1,2,3,4
+),
+
+b as 
+(
+SELECT
+        month,
+        city,
+        8 row_order,
+        'TOTAL' ltr_taxi,
+        '' recency_taxi,
+        SUM(existing_custr_count) existing_custr_count,
+        100.00 existing_custr_distr,
+        SUM(ao_users) ao_users,
+        COALESCE(try(SUM(ao_users)*100.00/SUM(existing_custr_count)), 0) ao_conversion,
+        SUM(fe_users) fe_users,
+        COALESCE(try(SUM(fe_users)*100.00/SUM(existing_custr_count)), 0) fe_conversion,
+        SUM(rr_users) rr_users,
+        COALESCE(try(SUM(rr_users)*100.00/SUM(existing_custr_count)), 0) rr_conversion,
+        COALESCE(try(SUM(rr_users)*100.00/SUM(fe_users)), 0) fe_rr,
+        SUM(net_users) net_users,
+        COALESCE(try(SUM(net_users)*100.00/SUM(existing_custr_count)), 0) net_conversion,
+        COALESCE(try(SUM(net_users)*100.00/SUM(rr_users)), 0) rr_net,
+        100.00 net_users_distr,
+        SUM(net_rides) net_rides,
+        100.00 net_rides_distr,
+        COALESCE(try(SUM(net_rides)*1.0/SUM(net_users)), 0) rpc,
+        SUM(discount_with_coin) discount_with_coin,
+        SUM(subtotal) subtotal,
+        COALESCE(try(SUM(discount_with_coin)*100.0/SUM(subtotal)), 0) discount_perc,
+        COALESCE(try(SUM(discount_with_coin)*1.0/SUM(net_rides)), 0) dpr
+FROM a
+GROUP BY 1,2
+),
+
+final as 
+(
+select * from a
+union
+select * from b
+)
+select 
+        month,
+        city,
+        ltr_taxi,recency_taxi,
+        existing_custr_count,existing_custr_distr,
+        ao_users,ao_conversion,
+        fe_users,fe_conversion,
+        rr_users,rr_conversion,fe_rr "fe_rr(users)",
+        net_users,net_conversion, rr_net "rr_net(users)", net_users_distr,
+        net_rides,net_rides_distr,
+        rpc,
+        -- discount_with_coin,
+        -- subtotal,
+        discount_perc,
+        dpr
+from final
+order by 1,2,row_order,4
---
+++ pricing/pricing-funnel-dataset/ocara_order.sql
@@ -0,0 +1,138 @@
+WITH city_cluster_hex AS (
+    
+        SELECT
+            cch.hex_id AS hex_id,
+            cch.cluster AS cluster
+        FROM
+            datasets.city_cluster_hex cch
+        WHERE
+            cch.resolution = 8
+    ),
+    
+    additive_surge AS (
+        
+        SELECT
+            srg_bkp.yyyymmdd AS yyyymmdd,
+            srg_bkp.service_detail_id AS service_detail_id,
+            srg_bkp.fare_estimate_id AS fare_estimate_id,
+            srg_bkp.price_increase_strategy AS additive_strategy
+        FROM
+            pricing.iprice_customer_surge_breakup srg_bkp
+        WHERE
+            srg_bkp.yyyymmdd >= DATE_FORMAT(DATE('{{StartDate}}'), '%Y%m%d') 
+            AND srg_bkp.yyyymmdd <= DATE_FORMAT(DATE('{{EndDate}}'), '%Y%m%d')
+            AND srg_bkp.surge_category = 'additive'
+    ),
+    
+    fare_estimates AS (
+
+        SELECT
+            'Route' AS geo_level,
+            'Cluster' AS geo_value,
+            fe_ench.epoch AS epoch,
+            fe_ench.yyyymmdd AS yyyymmdd,
+            fe_ench.city AS city_name,
+            fe_ench.service_name AS service_name,
+            fe_ench.service_detail_id AS service_detail_id,
+            fe_ench.pickup_location_hex_8 AS pickup_location_hex_8,
+            fe_ench.drop_location_hex_8 AS drop_location_hex_8,
+            COALESCE(fe_ench.price_increase_strategy,'no_dynamic_surge') AS surge_strategy,
+            COALESCE(fe_ench.is_gradient_applied,false) AS gradient_applied,
+            fe_ench.fare_estimate_id AS fare_estimate_id,
+            1 AS fe_count
+        FROM
+            pricing.fare_estimates_enriched fe_ench
+        WHERE
+            fe_ench.yyyymmdd >= DATE_FORMAT(DATE('{{StartDate}}'), '%Y%m%d') 
+            AND fe_ench.yyyymmdd <= DATE_FORMAT(DATE('{{EndDate}}'), '%Y%m%d')
+            AND fe_ench.service_name IN ('Link', 'Auto', 'Auto Pool', 'CabEconomy')
+    ),
+    
+    orders AS (
+    
+        SELECT
+            oli.yyyymmdd AS yyyymmdd,
+            oli.service_detail_id AS service_detail_id,
+            oli.estimate_id AS fare_estimate_id,
+            oli.order_status AS order_status,
+            oli.order_id AS order_id,
+            row_number() over (partition by oli.order_id order by oli.updated_epoch desc) as rank_1,
+            CASE 
+            WHEN oli.order_status in ('dropped') and oli.spd_fraud_flag != True then 'Net_Orders'
+            WHEN oli.order_status in ('customerCancelled') and oli.cancel_reason in ('order cancelled before rider accepted') then 'COBRA'
+            WHEN oli.order_status in ('customerCancelled') and oli.cancel_reason in ('Order cancelled before rider was mapped') then 'COBRM'
+            WHEN oli.order_status in ('customerCancelled') and oli.cancel_reason not in ('order cancelled before rider accepted', 'Order cancelled before rider was mapped') then 'OCARA'
+            ELSE 'Other' END AS end_state
+        FROM
+            orders.order_logs_immutable oli
+        WHERE
+            oli.yyyymmdd >= DATE_FORMAT(DATE('{{StartDate}}'), '%Y%m%d') 
+            AND oli.yyyymmdd <= DATE_FORMAT(DATE('{{EndDate}}'), '%Y%m%d')
+            AND oli.service_obj_service_name IN ('Link', 'Auto', 'Auto Pool', 'CabEconomy')
+    ),
+    
+    combined_df AS (
+        
+        SELECT 
+            fe.geo_level AS geo_level,
+            fe.geo_value AS geo_value,
+            fe.epoch AS epoch,
+            fe.yyyymmdd AS yyyymmdd,
+            fe.city_name AS city_name,
+            fe.service_name AS service_name,
+            fe.service_detail_id AS service_detail_id,
+            pic.cluster AS pickup_location,
+            drp.cluster AS drop_location,
+            fe.surge_strategy AS surge_strategy,
+            fe.gradient_applied AS gradient_applied,
+            COALESCE(add_srg.additive_strategy,'no_additive_surge') AS additive_strategy,
+            fe.fare_estimate_id AS fare_estimate_id,
+            ord.order_id AS order_id,
+            ord.rank_1 AS rank_1,
+            ord.end_state AS end_state
+
+        FROM
+            fare_estimates fe
+        LEFT JOIN
+            city_cluster_hex pic
+            ON fe.pickup_location_hex_8 = pic.hex_id
+        LEFT JOIN 
+            city_cluster_hex drp
+            ON fe.drop_location_hex_8 = drp.hex_id
+        LEFT JOIN
+            additive_surge add_srg
+            ON fe.yyyymmdd = add_srg.yyyymmdd
+            AND fe.service_detail_id = add_srg.service_detail_id
+            AND fe.fare_estimate_id = add_srg.fare_estimate_id
+        LEFT JOIN
+            orders ord
+            ON fe.yyyymmdd = ord.yyyymmdd
+            AND fe.service_detail_id = ord.service_detail_id
+            AND fe.fare_estimate_id = ord.fare_estimate_id
+    )
+    
+SELECT 
+    cdf.geo_level AS geo_level,
+    cdf.geo_value AS geo_value,
+    cdf.city_name AS city_name,
+    cdf.service_name AS service_name,
+    cdf.service_detail_id AS service_detail_id,
+    cdf.pickup_location AS pickup_location,
+    cdf.drop_location AS drop_location,
+    cdf.surge_strategy AS surge_strategy,
+    cdf.gradient_applied AS gradient_applied,
+    cdf.additive_strategy AS additive_strategy,
+    COUNT(DISTINCT CASE WHEN cdf.end_state = 'OCARA' AND cdf.rank_1 = 1 THEN cdf.order_id END) AS ocara_orders
+FROM
+    combined_df cdf
+GROUP BY
+    geo_level,
+    geo_value,
+    city_name,
+    service_name,
+    service_detail_id,
+    pickup_location,
+    drop_location,
+    surge_strategy,
+    gradient_applied,
+    additive_strategy
